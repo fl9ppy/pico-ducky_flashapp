2f0f4f9edc369d2cd67883c1889ec775bb3873c2
fl9ppy <91787965+fl9ppy@users.noreply.github.com>
2023-04-01 14:32:43 +0300

    lil fixes
    am pus si comentarii, si am mai completat un pic

--- back-end.py
+++ back-end.py
@@ -3,8 +3,10 @@ import shutil
 import time
 import stat
 
-base_path = input("Path to repo: ")
 
+
+#constructing paths
+base_path = input("Path to repo: ")
 pico_nuke = input("Path to pico: ")
 nuke = base_path+'/flash_nuke.uf2'
 circuit_python = base_path+'/circuit_python.uf2'
@@ -13,18 +15,26 @@ asyncio = base_path+'/lib_aux/asyncio//'
 wsgi = base_path+'/lib_aux/adafruit_wsgi//'
 hid = base_path+'/lib_aux/adafruit_hid//'
 
-
+#nuke the pico and install circuitpython
 shutil.copy2(nuke, pico_nuke)
 time.sleep(10)
 next = input("continue? y/n: ")
-if next == "y":
+if next == "y" or next == "Y":
     shutil.copy2(circuit_python, pico_nuke)
     time.sleep(10)
+else:
+    exit()
+
+#moving libraries from the repo to the pico
 pico = input("Path to pico (After nuking it can change): ")
 pico_lib = pico+'/lib'
 shutil.rmtree(pico_lib)
 time.sleep(10)
 shutil.move(local_lib, pico)
+shutil.copy2(base_path+'/lib_aux/adafruit_debouncer.mpy', base_path+'/lib')
+shutil.copy2(base_path+'/lib_aux/adafruit_ticks.mpy', base_path+'/lib')
+
+#re-creating another lib in the repo (because the original folder was moved to the pico) and re-creating it's subfolders
 os.mkdir(base_path+'/lib')
 time.sleep(10)
 os.mkdir(base_path+'/lib/asyncio')
@@ -32,6 +42,10 @@ os.mkdir(base_path+'/lib/adafruit_wsgi')
 os.mkdir(base_path+'/lib/adafruit_hid')
 time.sleep(10)
 
+
+#copying everything from the aux lib in the new lib
+#(This process its needed because shutil can't copy files to the pico, so we use an auxiliary lib in the repo, 
+#from whom we copy the files to the new lib so we still have everything for multiple uses)
 for file_name in os.listdir(asyncio):
     source = asyncio + file_name
     destination = base_path+'/lib/asyncio//' + file_name
@@ -50,8 +64,12 @@ for file_name in os.listdir(hid):
     if os.path.isfile(source):
         shutil.copy2(source, destination)
 
-shutil.copy2(base_path+'/lib_aux/adafruit_debouncer.mpy', base_path+'/lib')
-shutil.copy2(base_path+'/lib_aux/adafruit_ticks.mpy', base_path+'/lib')
+
+
+
+
+
+
 
 print("DONE")
 
