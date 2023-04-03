1cbc8bcf7c258d8259f0dcf41692aa88b5067b8f
alexdobre <alx.dobre@gmail.com>
2023-04-03 15:43:45 +0100

    Nuke works

--- app.py
+++ app.py
@@ -14,42 +14,42 @@ root.resizable(False, False)
 
 title = tk.Label(text="Pico-Ducky creator")
 
-l1 = tk.Label(text="Select the folder where you cloned the repo")
-l2 = tk.Label(text="Select the location of the pico")
+base_label = tk.Label(text="Select the folder where you cloned the repo")
+nuke_label = tk.Label(text="Select the location of the pico")
 
-path1 = tk.Entry()
-path2 = tk.Entry()
+base_path = tk.Entry()
+nuke_path = tk.Entry()
 
 def repo_select():
-    x = filedialog.askdirectory()
-    path1.insert(0, x)
+   x = filedialog.askdirectory()
+   base_path.insert(0, x)
 
 def pico_location():
-    y = filedialog.askdirectory()
-    path2.insert(0, y)
+   y = filedialog.askdirectory()
+   nuke_path.insert(0, y)
 
-#constructing paths
 
-base_path = path1.get()
-pico_nuke = path2.get()
-print(base_path)
-
-nuke = base_path+'/flash_nuke.uf2'
-circuit_python = base_path+'/circuit_python.uf2'
-local_lib = base_path+'/lib'
-asyncio = base_path+'/lib_aux/asyncio//'
-wsgi = base_path+'/lib_aux/adafruit_wsgi//'
-hid = base_path+'/lib_aux/adafruit_hid//'
-res = base_path+'/res//'
+def build_paths():
+    return {
+        'nuke': base_path.get() + '/flash_nuke.uf2',
+        'circuit_python': base_path.get() + '/circuit_python.uf2',
+        'local_lib': base_path.get() + '/lib',
+        'asyncio': base_path.get() + '/lib_aux/asyncio//',
+        'wsgi': base_path.get() + '/lib_aux/adafruit_wsgi//',
+        'hid': base_path.get() + '/lib_aux/adafruit_hid//',
+        'res': base_path.get() + '/res//'
+    }
 
 #nuke the pico and install circuitpython
 def nuke_fun():
 
-    shutil.copy2(nuke, pico_nuke)
+    paths = build_paths()
+
+    shutil.copy2(paths['nuke'], nuke_path.get())
     time.sleep(10)
     nuked = askyesno(title="Sure?", message="Pico has been nuked, continue?")
     if nuked:
-        shutil.copy2(circuit_python, pico_nuke)
+        shutil.copy2(paths['circuit_python'], nuke_path.get())
         time.sleep(10)
     else:
         root.destroy()
@@ -57,11 +57,13 @@ def nuke_fun():
 
 def flash():
 
+    paths = build_paths()
+
     root2 = tk.Tk()
     root2.geometry("400x200")
     root2.resizable(False, False)
-    l3 = tk.Label(roo2, text="Select the pico path again (After installing circuitpython the path might change)")
-    def pico_location2():
+    l3 = tk.Label(root2, text="Select the pico path again (After installing circuitpython the path might change)")
+    def pico_location():
         pico = filedialog.askdirectory()
         time.sleep(3)
         root2.destroy()
@@ -70,14 +72,14 @@ def flash():
     root2.mainloop
     time.sleep(3)
 
-#moving libraries from the repo to the pico
+    # moving libraries from the repo to the pico
     pico_lib = pico+'/lib'
     shutil.rmtree(pico_lib)
     time.sleep(10)
     shutil.move(local_lib, pico)
 
 
- #re-creating another lib in the repo (because the original folder was moved to the pico) and re-creating it's subfolders
+    # re-creating another lib in the repo (because the original folder was moved to the pico) and re-creating it's subfolders
     os.mkdir(base_path+'/lib')
     time.sleep(10)
     os.mkdir(base_path+'/lib/asyncio')
@@ -85,9 +87,9 @@ def flash():
     os.mkdir(base_path+'/lib/adafruit_hid')
     time.sleep(10)
 
-#copying everything from the aux lib in the new lib
-#(This process its needed because shutil can't copy files to the pico, so we use an auxiliary lib in the repo, 
-#from whom we copy the files to the new lib so we still have everything for multiple uses)
+    # copying everything from the aux lib in the new lib
+    # (This process its needed because shutil can't copy files to the pico, so we use an auxiliary lib in the repo,
+    # from whom we copy the files to the new lib so we still have everything for multiple uses)
     for file_name in os.listdir(asyncio):
         source = asyncio + file_name
         destination = base_path+'/lib/asyncio//' + file_name
@@ -109,7 +111,7 @@ def flash():
     shutil.copy2(base_path+'/lib_aux/adafruit_debouncer.mpy', base_path+'/lib')
     shutil.copy2(base_path+'/lib_aux/adafruit_ticks.mpy', base_path+'/lib')
 
-#copying the last files to the pico
+    # copying the last files to the pico
     os.remove(pico+'code.py')
     for file_name in os.listdir(res):
         source = res + file_name
@@ -117,18 +119,18 @@ def flash():
         if os.path.isfile(source):
             shutil.copy2(source, destination)
 
-#select_base_path = tk.Button(text="Select", command=repo_select)
-#select_pico_nuke = tk.Button(text="Select", command=pico_location)
-format_pico  = tk.Button(text="Nuke", command=nuke_fun)
+select_base_path = tk.Button(text="Select", command=repo_select)
+select_pico_nuke = tk.Button(text="Select", command=pico_location)
+format_pico = tk.Button(text="Nuke", command=nuke_fun)
 flash_button = tk.Button(text="Flash", command=flash)
 
 title.pack()
-l1.place(x=30, y=120)
-#select_base_path.place(x=30, y=150)
-path1.place(x=80, y=153)
-l2.place(x=30, y=220)
-#select_pico_nuke.place(x=30, y=250)
-path2.place(x=80, y=253)
+base_label.place(x=30, y=120)
+select_base_path.place(x=30, y=150)
+base_path.place(x=80, y=153)
+nuke_label.place(x=30, y=220)
+select_pico_nuke.place(x=30, y=250)
+nuke_path.place(x=80, y=253)
 format_pico.place(x=423, y=470)
 flash_button.place(x=423, y=500)
 root.mainloop()
